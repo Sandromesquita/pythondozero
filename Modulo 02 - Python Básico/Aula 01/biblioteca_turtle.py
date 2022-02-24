98c7759220d71dc46203ba3819b8d85d67f4bdd7
Sandro Mesquita <39969611+Sandromesquita@users.noreply.github.com>
2022-02-24 15:40:45 -0300

    Aula 02 - Usando a biblioteca Turtle

---
+++ Modulo 02 - Python Básico/Aula 01/biblioteca_turtle.py
@@ -0,0 +1,19 @@
+import turtle
+
+tela = turtle.Screen()
+tela.bgcolor("green")
+
+desenhista = turtle.Turtle()
+desenhista.forward(100)
+desenhista.left(60)
+desenhista.forward(100)
+desenhista.left(60)
+desenhista.forward(100)
+desenhista.left(60)
+desenhista.forward(100)
+desenhista.left(60)
+desenhista.forward(100)
+desenhista.left(60)
+desenhista.forward(100)
+
+tela.exitonclick()
